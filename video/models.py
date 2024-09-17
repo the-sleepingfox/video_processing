fd7821fd8b73907ffc2a2e56ca84f2c8155b70bf
the-sleepingfox <ayushtripathi.india@gmail.com>
2024-09-17 20:10:47 +0530

    Home page setup

--- video/models.py
+++ video/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    video_title= models.CharField(max_length=255)
+    video_file= models.FileField(upload_to='videos/')
+    uploaded_at= models.DateTimeField(auto_now_add=True)
+    processed_flag= models.BooleanField(default=False)
+
+class Subtitle(models.Model):
+    video= models.ForeignKey(Video, on_delete= models.CASCADE)
+    language= models.CharField(max_length=10, default='en')
+    text= models.TextField()
+    timestamp= models.TimeField() #this timestamp stores the time value for pharase ocurrence time
